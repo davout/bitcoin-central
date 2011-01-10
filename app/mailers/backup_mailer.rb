981402c8d9e336f0a5cd691c4f30df6e79eb5904
David FRANCOIS <david.francois@webflows.fr>
2011-01-10 11:40:36 +0100

    Well, it doesn't :/

--- app/mailers/backup_mailer.rb
+++ app/mailers/backup_mailer.rb
@@ -2,11 +2,11 @@ class BackupMailer < ActionMailer::Base
   default :from => "Bitcoin Central support <support@bitcoin-central.net>"
 
   def wallet_backup(recipient, wallet)
-    mail :to => recipient,
-      :subject => "Wallet backup #{DateTime.now.strftime("%d/%m/%Y %H:%M")}"
-    
     attachments.inline['bitcoin.png'] = File.read(File.join(Rails.root, "public", "images", "bitcoin.png"))
 
     attachments["wallet_#{DateTime.now.strftime("%Y_%m_%d_%H_%M.dat.gpg")}"] = File.read(wallet)
+
+    mail :to => recipient,
+      :subject => "Wallet backup #{DateTime.now.strftime("%d/%m/%Y %H:%M")}"
   end
 end
