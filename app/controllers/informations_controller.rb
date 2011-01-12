dd3d744a10aa4a2cf2223d81709a50966b95506e
David FRANCOIS <david.francois@webflows.fr>
2011-01-12 15:10:18 +0100

    Graphing only the last 48 hours of trading

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -5,11 +5,17 @@ class InformationsController < ApplicationController
     @min_y = [0.3 * (Trade.minimum(:ppc) or 0) - (Trade.maximum(:ppc) or 0), 0].max
     @max_y = 1.3 * (Trade.maximum(:ppc) or 0)
 
+    @max_x = Trade.maximum(:created_at)
+    @min_x = @max_x.advance(:hours => -48)
+
     @series = []
     @options = jqchart_defaults
 
     @options[:axes][:yaxis][:min] = @min_y
     @options[:axes][:yaxis][:max] = @max_y
+
+    @options[:axes][:xaxis][:min] = @min_x.strftime("%Y-%m-%d %H:%M:%S")
+    @options[:axes][:xaxis][:max] = @max_x.strftime("%Y-%m-%d %H:%M:%S")
 
     %w{LRUSD LREUR EUR}.each do |currency|
       @series << Trade.with_currency(currency).map do |trade|
