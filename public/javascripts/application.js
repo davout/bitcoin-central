788d650041ee04f601a082c6616eb030679590c8
David FRANCOIS <david.francois@webflows.fr>
2011-01-15 02:04:30 +0100

    Friendlier and more convenient trade order creation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,58 @@
-function wizardNextStep(elt) {
-    elt = $(elt);
+$(document).ready(function() {
+    // Triggered by a currency or category selection on
+    // the trade order creation form
+    // TODO : DRY this up with body class/id
+    $("input.currency-select").click(function(e) {
+        updateTradeOrderForm();
+    });
 
-    step = elt.up("div.transfer-wizard").id;
+    $("input.category-select").click(function(e) {
+        updateTradeOrderForm();
+    });
+});
 
-    switch (step) {
-        case "category-step":
-            $('category').value = elt.id;
-            $('category-step').hide();
-            $('currency-step').show();
 
-            break;
+function updateTradeOrderForm() {
+    currency = $("input:radio.currency-select:checked").val();
+    category = $("input:radio.category-select:checked").val();
+    
 
-        case "currency-step":
-            $('currency').value = elt.id;
-            $('currency-step').hide();
+    if (category) {
+        setLinksFor(category);
 
-            $($('category').value + "-" + $('currency').value).show();
-
-            break;
+        if (category == "sell") {
+           setBalance("BTC");
+        }
+        else {
+            if (currency) {
+                setBalance(currency);
+            }
+            else {
+                $("#balance").val("");
+            }
+        }
     }
 }
+
+function setBalance(currency) {
+    $.get("/user/balance", { "currency" : currency }, function(data) {
+            balance = $("#balance").val(data + " " + currency);
+        }
+    );
+}
+
+function setLinksFor(category) {
+    $(".js-calculation").each(function(idx, e) { $(e).hide(); });
+    $(".js-calculation." + category).each(function(idx, e) { $(e).show(); });
+}
+
+
+
+
+
+
+
+
+
+
+
