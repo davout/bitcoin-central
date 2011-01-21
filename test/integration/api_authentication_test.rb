c1afdc26c892f276a010f85e3f0b90019de5b47c
David FRANCOIS <david.francois@webflows.fr>
2011-01-21 12:30:28 +0100

    Test stubs for API authentication

---
+++ test/integration/api_authentication_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class ApiAuthenticationTest < ActionDispatch::IntegrationTest
+  fixtures :all
+  
+  test "authentication should fail with and old token" do
+    flunk
+  end
+
+  test "authentication should fail with an invalid hash" do
+    flunk
+  end
+
+  test "get account history in XML using API" do
+    flunk
+  end
+
+  test "post a trade order using XML API" do
+    flunk
+  end
+
+  test "post a trade order using JSON API" do
+    flunk
+  end
+end
