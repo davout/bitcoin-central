2a7b1382d31d3327cb329b463f06f538de36f8ed
David FRANCOIS <david.francois@webflows.fr>
2011-05-13 00:20:43 +0200

    Yummy HAML is yummy

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,5 +1,19 @@
 module AccountsHelper
-  def exact_balance_title(currency)
-    "title=\"#{"%.5f" % @current_user.balance(currency)} #{currency.to_s.upcase}\"".html_safe
+  def exact_balance(user, currency)
+    "#{"%.5f" % user.balance(currency)} #{currency.to_s.upcase}\""
+  end
+
+  def unconfirmed_btc_balance_part(user)
+    user.balance(:btc, :unconfirmed => true) - user.balance(:btc)
+  end
+
+  def color_for_balance(user, currency)
+    "green" if user.balance(currency) > 0
+    "red"   if user.balance(currency) < 0
+  end
+
+  def sign_for_balance(user, currency)
+    "+" if user.balance(currency) > 0
+    "-" if user.balance(currency) < 0
   end
 end
