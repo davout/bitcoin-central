6b273e883f955596f80422e53eb836e9a75ed4f0
David FRANCOIS <david.francois@webflows.fr>
2011-05-24 16:55:02 +0200

    Update secret token so Devise works again

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BitcoinBank::Application.config.secret_token = '1c8d72f49c79fbecef27d20efe9175f48e20c18d0ffe1059910e24023b5ff6dd2247d5434054bb33864d5e2c06fc89cb6b6e48682d7b40a39042cbbedbf9f0a5'
+BitcoinBank::Application.config.secret_token = '1c8d72f49c79fbecef27d20efokzegjeznze654zef2zef651dcdv54dfv21v6582247d5434054bb33864d5e2c06fc89cb6b6e48682d7b40a39042cbbedbf9f0a5'
