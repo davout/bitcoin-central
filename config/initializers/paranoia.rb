4e91e20503524e6acc0bb42a3dc6eb543868364a
David FRANCOIS <david.francois@webflows.fr>
2011-06-22 00:33:57 +0200

    Maintenance, 404 and 500 pages, fixes #61 and #62

--- config/initializers/paranoia.rb
+++ config/initializers/paranoia.rb
@@ -1,2 +1,5 @@
 # This forces every model to explicitly whitelist accessible attributes
 ActiveRecord::Base.send(:attr_accessible, nil)
+
+# This is necessary to prevent errors when using AR's DB session store
+ActiveRecord::Base.send(:attr_accessible, :session_id)
