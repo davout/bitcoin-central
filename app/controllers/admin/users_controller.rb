ca10ab48aa51ffb73d3024973d8f7366a628c6cc
David FRANCOIS <david.francois@webflows.fr>
2011-06-30 18:10:03 +0200

    User admin allows for require_otp flag to be changed

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,7 @@ class Admin::UsersController < Admin::AdminController
       :account,
       :email,
       :admin,
+      :require_otp,
       :time_zone,
       :last_address,
       :confirmation_sent_at,
@@ -17,11 +18,35 @@ class Admin::UsersController < Admin::AdminController
       :remember_created_at,
       :transfers
     ]
-
-    config.list.columns = config.update.columns = [
+    
+    config.list.columns = [
       :account,
       :email,
       :admin
+    ]
+      
+    config.update.columns = [
+      :account,
+      :email,
+      :admin,
+      :require_otp
+    ]
+    
+    config.show.columns = [
+      :account,
+      :email,
+      :admin,
+      :require_otp,
+      :time_zone,
+      :last_address,
+      :confirmation_sent_at,
+      :confirmed_at,
+      :current_sign_in_at,
+      :current_sign_in_ip,
+      :failed_attempts,
+      :last_sign_in_at,
+      :last_sign_in_ip,
+      :locked_at,
     ]
     
     config.nested.add_link :transfers
