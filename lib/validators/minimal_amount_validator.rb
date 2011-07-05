dc57ff93ee05e27ebb4c66627369e8d1268328b0
David FRANCOIS <david.francois@webflows.fr>
2011-07-05 23:53:39 +0200

    Any size of incoming BTC transaction should be acceptable

--- lib/validators/minimal_amount_validator.rb
+++ lib/validators/minimal_amount_validator.rb
@@ -1,6 +1,6 @@
 class MinimalAmountValidator < ActiveModel::EachValidator
   def validate_each(record, field, value)
-    if value and (value.abs < 0.001) and !record.skip_min_amount
+    if value and (value.abs < 0.01) and !record.skip_min_amount and record.new_record?
       record.errors[field] << (I18n.t "errors.min_amount", :minimum=>0.001, :currency=>record.currency)
     end
   end
--- test/unit/bitcoin_transfer_test.rb
+++ test/unit/bitcoin_transfer_test.rb
@@ -11,4 +11,32 @@ class BitcoinTransferTest < ActiveSupport::TestCase
     transfer.user.expects(:generate_new_address).at_least_once
     assert transfer.save
   end
+
+  test "transaction sync should handle small txes without problem" do
+    tx = {
+      "category" => "receive",
+      "amount" => 0.00001,
+      "confirmations" => 35414,
+      "txid" => "0e0e9fec1195132b28662539528e936c981ebd3b0f140d9ad3c65717cf2f329a"
+    }  
+    
+    Bitcoin::Client.instance.
+      stubs(:list_transactions).
+      returns([tx], [tx])
+    
+    User.stubs(:all).returns(User.where("id = ?", users(:trader1).id))
+
+    assert_difference 'users(:trader1).balance(:btc)', 0.00001.to_d do
+      assert_difference 'Transfer.count' do
+        BitcoinTransfer.synchronize_transactions!
+      end
+    end
+
+    # It should then not fail when updating the same transaction
+    assert_no_difference 'users(:trader1).balance(:btc)' do
+      assert_no_difference 'Transfer.count' do
+        BitcoinTransfer.synchronize_transactions!
+      end
+    end
+  end
 end
