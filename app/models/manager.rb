e162c90c244608a3d3c4c4c28a67e814fff6c5dd
David FRANCOIS <david.francois@webflows.fr>
2011-08-23 13:41:18 +0200

    Currency management fix

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,7 +1,7 @@
 class Manager < User
   def allowed_currencies
     Currency.
-      where("id IN (#{used_currencies.where(:management => true).map(&:id).join(",")})").
+      where("id IN (#{used_currencies.where(:management => true).map(&:currency_id).join(",")})").
       map { |c| c.code.downcase.to_sym }
   end
 end
