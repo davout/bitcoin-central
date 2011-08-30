50384a118ebaee8929bd17c5c98d0afc099b28db
David FRANCOIS <david.francois@webflows.fr>
2011-08-30 10:34:06 +0200

    Currencies admin fix

--- app/controllers/admin/currencies_controller.rb
+++ app/controllers/admin/currencies_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CurrenciesController < Admin::AdminController
   before_filter :enforce_admin_rights
   
-  active_scaffold :currency
+  active_scaffold :currency do |config|
+    config.columns = [:code, :created_at]
+    config.actions.exclude :update, :delete, :show
+  end
 end
