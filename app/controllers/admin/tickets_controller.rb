10dce22652121750040fccd45d31b9e5d16cc494
David FRANCOIS <david.francois@webflows.fr>
2011-09-29 11:19:03 +0200

    We only want to see pending support tickets

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -2,4 +2,9 @@ class Admin::TicketsController < Admin::AdminController
   active_scaffold :ticket do |config|
     config.columns = [:id, :state, :user, :created_at, :title, :description]
   end
+  
+  # We only want to see pending tickets
+  def conditions_for_collection
+    ["state = 'pending'"]
+  end
 end
