fd539f5b17b01b3c78f5fec1a871fe27e3ce3d3b
David FRANCOIS <david.francois@webflows.fr>
2011-09-30 22:54:01 +0200

    Revert ticket filtering

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -2,9 +2,4 @@ class Admin::TicketsController < Admin::AdminController
   active_scaffold :ticket do |config|
     config.columns = [:id, :state, :user, :created_at, :title, :description]
   end
-  
-  # We only want to see pending tickets
-  def conditions_for_collection
-    ["state = 'pending'"]
-  end
 end
