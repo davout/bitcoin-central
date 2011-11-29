11195dc12cfa3a2dc1fb846ee475812790eaf054
David FRANCOIS <david.francois@webflows.fr>
2011-11-29 16:32:51 +0100

    Fixed a test

--- test/unit/account_operation_test.rb
+++ test/unit/account_operation_test.rb
@@ -81,10 +81,9 @@ class AccountOperationTest < ActiveSupport::TestCase
     assert_equal BigDecimal("10.0"), user.balance(:lreur)
   end
   
-  
   # We want to make sure the JSON serialization does not include the model name as root
   test "serialization should not include root model name" do
-    assert_equal AccountOperation.new.to_json({}),
-      "{\"comment\":null,\"bt_tx_id\":null,\"address\":null,\"created_at\":null,\"unread\":null,\"amount\":0.0,\"bt_tx_confirmations\":0,\"currency\":null,\"email\":null}"
+    assert_equal JSON::parse(AccountOperation.new.to_json({})),
+      JSON::parse("{\"comment\":null,\"bt_tx_id\":null,\"address\":null,\"created_at\":null,\"unread\":null,\"amount\":0.0,\"bt_tx_confirmations\":0,\"currency\":null,\"email\":null}")
   end
 end
