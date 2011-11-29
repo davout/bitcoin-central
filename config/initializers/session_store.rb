6376edfe46f715b465546a9f09e5befa128e4d48
David FRANCOIS <david.francois@webflows.fr>
2011-11-29 13:30:51 +0100

    It now works properly without breaking tests

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,1 @@
-domain = Rails.configuration.base_url.split(":\/\/")[1].gsub(/(\:\d+)?\/?/, "")
-BitcoinBank::Application.config.session_store :active_record_store, :key => "bc-session", :domain => domain
+BitcoinBank::Application.config.session_store :active_record_store, :key => "bc-session", :domain => :all
