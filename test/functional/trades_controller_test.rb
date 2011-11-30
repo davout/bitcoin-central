e1db2e5979910dfab8d4de301c827e5cf108ad2e
David FRANCOIS <david.francois@webflows.fr>
2011-11-30 11:46:16 +0100

    Failing test for spotted trade orders list page bug

--- test/functional/trades_controller_test.rb
+++ test/functional/trades_controller_test.rb
@@ -9,4 +9,35 @@ class TradesControllerTest < ActionController::TestCase
       assert_response :success
     end
   end
+  
+  test "should render trades list" do
+    t1 = Factory(:user)
+    t2 = Factory(:user)
+    
+    add_money(t1, 1000, :btc)
+    add_money(t2, 1000, :eur)
+    
+    Factory(:limit_order,
+      :amount => 1,
+      :category => "sell",
+      :ppc => 1,
+      :user => t1,
+      :currency => "EUR"
+    )
+    
+    t = Factory(:market_order,
+      :amount => 1,
+      :category => "buy",
+      :user => t2,
+      :currency => "EUR"
+    )  
+      
+    assert_difference "Trade.count" do
+      t.execute!
+    end
+    
+    login_with(Factory(:user))
+    get :index
+    assert_response :success
+  end
 end
