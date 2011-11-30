4141adb58f2f5ad15e1f961a571574649c1695ee
David FRANCOIS <david.francois@webflows.fr>
2011-11-30 10:34:17 +0100

    Migration fix

--- db/migrate/20111129123247_add_max_read_id_to_users.rb
+++ db/migrate/20111129123247_add_max_read_id_to_users.rb
@@ -1,6 +1,9 @@
 class AddMaxReadIdToUsers < ActiveRecord::Migration
   def self.up
-    add_column :accounts, :max_read_tx_id, :integer    
+    add_column :accounts, :max_read_tx_id, :integer,
+      :null => false,
+      :default => 0
+    
     execute 'UPDATE `accounts` SET `max_read_tx_id` = (SELECT MAX(id) FROM `account_operations` WHERE `account_operations`.`account_id` = `accounts`.`id`)'
   end
 
