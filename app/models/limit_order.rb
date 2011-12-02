d4ab31358c3112d3f4eea6eb02a02aee84d2ecb5
David FRANCOIS <david.francois@webflows.fr>
2011-12-02 08:44:43 +0100

    Trade orders behaviour fixed on the edge

--- app/models/limit_order.rb
+++ app/models/limit_order.rb
@@ -1,6 +1,4 @@
 class LimitOrder < TradeOrder
-  attr_accessible :ppc
-
   validates :ppc,
     :minimal_order_ppc => true,
     :numericality => true
--- test/unit/trade_test.rb
+++ test/unit/trade_test.rb
@@ -1,4 +1,37 @@
 require 'test_helper'
 
 class TradeTest < ActiveSupport::TestCase
+  test "trade shouldn't happen with traded btc or currency being zero" do
+    u = Factory(:user)
+    to = Factory.build(:limit_order,
+      :category => "sell",
+      :currency => "EUR",
+      :user_id => "0"
+    )
+    
+    to.save :validate => false
+    
+    t = Trade.new do |t|
+      t.purchase_order = to
+      t.sale_order = to
+      t.seller = u
+      t.buyer = u
+      t.currency = "EUR"
+    end
+    
+    assert !t.valid?
+    
+    t.traded_btc = 1
+    
+    assert !t.valid?
+    
+    t.traded_btc = 0
+    t.traded_currency = 1
+    
+    assert !t.valid?
+    
+    t.traded_btc = 1
+    
+    assert t.valid?
+  end
 end
